c3da85d0c3d0ba83fb87abb31189f9606b668f66
suparthghimire <suparthnarayanghimire2014@gmail.com>
2022-11-14 19:23:05 +0545

    Auth Route Endpoint Setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 
+/* Route Imports */
+import AuthRoutes from "./routes/auth";
+
 dotenv.config();
 const app = express();
 
@@ -11,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
-app.get("/", (req, res) => res.status(200).json({ message: "OK" }));
+app.use("/", AuthRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
---
+++ server/src/routes/auth.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+
+import AuthController from "../controllers/AuthController";
+
+router.get("/", AuthController.index); // User Data
+router.post("/signin", AuthController.signIn); //Login
+router.post("/signup", AuthController.signUp); //Register
+router.delete("/signout", AuthController.signOut); //Logout
+
+export default router;
