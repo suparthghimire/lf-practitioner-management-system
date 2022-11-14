46890a8e6bedc4e5ef4bcb3888b661c89d422e60
suparthghimire <suparthnarayanghimire2014@gmail.com>
2022-11-14 19:06:23 +0545

    Express Server Initialization

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.get("/", (req, res) => res.status(200).json({ message: "OK" }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
