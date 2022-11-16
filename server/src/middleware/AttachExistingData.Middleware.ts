a7188460305a8150a6adafe68a6e408311b31ef5
suparthghimire <suparthnarayanghimire2014@gmail.com>
2022-11-16 21:58:22 +0545

    Add Check for Empty Body Keys

--- server/src/middleware/AttachExistingData.Middleware.ts
+++ server/src/middleware/AttachExistingData.Middleware.ts
@@ -16,62 +16,73 @@ export default async function AttachExistingData(
   try {
     const { Specializations, WorkingDays } = req.body;
     if (!Specializations) req.body.Specializations = [];
+    if (!WorkingDays) req.body.WorkingDays = [];
 
-    // variable that will store final attached data of specializations
-    let newSpecialization: Specialization[] = [];
+    // If there are no specializations and workingDays, then we don't need to do anything. Just move on to next middleware
+    if (
+      req.body.Specializations.length <= 0 &&
+      req.body.WorkingDays.length <= 0
+    )
+      next();
+    else {
+      // variable that will store final attached data of specializations
+      let newSpecialization: Specialization[] = [];
 
-    // variable that will store final attached data of workingDays
-    let newWorkingDays: Day[] = [];
+      // variable that will store final attached data of workingDays
+      let newWorkingDays: Day[] = [];
 
-    type SpecsPromise = Promise<Specialization[] | undefined>;
-    type WorkDayPromise = Promise<Day[] | undefined>;
+      type SpecsPromise = Promise<Specialization[] | undefined>;
+      type WorkDayPromise = Promise<Day[] | undefined>;
 
-    //variables to hold promises as they can run together and then we can await them together
-    let unresolvedSpecs: SpecsPromise =
-      Promise.resolve() as unknown as SpecsPromise;
-    let unresolvedWorkDays: WorkDayPromise =
-      Promise.resolve() as unknown as WorkDayPromise;
+      //variables to hold promises as they can run together and then we can await them together
+      let unresolvedSpecs: SpecsPromise =
+        Promise.resolve() as unknown as SpecsPromise;
+      let unresolvedWorkDays: WorkDayPromise =
+        Promise.resolve() as unknown as WorkDayPromise;
 
-    // if specialization is present in request body
-    if (Specializations) {
-      // loop through all the specializations and parse them to fit the Specialization Schema
-      newSpecialization = Specializations.map(
-        (spec: { id: string; name: string }) => ({
-          id: parseInt(spec.id),
-          name: spec.name,
-        })
-      );
-      // Fetches the replaced data from database of specializations
-      unresolvedSpecs =
-        SpecializationService.attachExistingSpecializationToNew(
-          newSpecialization
+      // if specialization is present in request body
+      if (Specializations) {
+        // loop through all the specializations and parse them to fit the Specialization Schema
+        newSpecialization = Specializations.map(
+          (spec: { id: string; name: string }) => ({
+            id: parseInt(spec.id),
+            name: spec.name,
+          })
         );
-    }
-    // if WOrkingDays is present in request body
-    if (WorkingDays) {
-      // loop through all the working days and parse them to fit the WorkingDay Schema
-      newWorkingDays = WorkingDays.map((day: { id: string; day: string }) => ({
-        id: parseInt(day.id),
-        day: day.day,
-      }));
-      // Fetches the replaced data from database of working days
-      unresolvedWorkDays =
-        WorkingDayService.attachExistingDayToNew(newWorkingDays);
-    }
+        // Fetches the replaced data from database of specializations
+        unresolvedSpecs =
+          SpecializationService.attachExistingSpecializationToNew(
+            newSpecialization
+          );
+      }
+      // if WOrkingDays is present in request body
+      if (WorkingDays) {
+        // loop through all the working days and parse them to fit the WorkingDay Schema
+        newWorkingDays = WorkingDays.map(
+          (day: { id: string; day: string }) => ({
+            id: parseInt(day.id),
+            day: day.day,
+          })
+        );
+        // Fetches the replaced data from database of working days
+        unresolvedWorkDays =
+          WorkingDayService.attachExistingDayToNew(newWorkingDays);
+      }
 
-    // await for all the promises to resolve
-    const [attachedSpecializations, attachedWorkingDays] = await Promise.all([
-      unresolvedSpecs,
-      unresolvedWorkDays,
-    ]);
-    // if attachedSpecializations is present, replace the Specializations in request body with attachedSpecializations
-    if (attachedSpecializations)
-      req.body.Specializations = attachedSpecializations;
-    // if attachedWorkingDays is present, replace the WorkingDays in request body with attachedWorkingDays
-    if (attachedWorkingDays) req.body.WorkingDays = attachedWorkingDays;
+      // await for all the promises to resolve
+      const [attachedSpecializations, attachedWorkingDays] = await Promise.all([
+        unresolvedSpecs,
+        unresolvedWorkDays,
+      ]);
+      // if attachedSpecializations is present, replace the Specializations in request body with attachedSpecializations
+      if (attachedSpecializations)
+        req.body.Specializations = attachedSpecializations;
+      // if attachedWorkingDays is present, replace the WorkingDays in request body with attachedWorkingDays
+      if (attachedWorkingDays) req.body.WorkingDays = attachedWorkingDays;
 
-    // call next middleware
-    next();
+      // call next middleware
+      next();
+    }
   } catch (error) {
     // return faiure
     console.log(error);
